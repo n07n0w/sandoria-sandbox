01801073c12da2a236cdf777b79f294bc5357ac7
Vasyl Shevchuk <vasyl@nuvolagroup.com.au>
2025-03-26 03:41:10 +0200

    Clear code

--- dbConfig.js
+++ dbConfig.js
@@ -4,7 +4,6 @@ function parseJawsDBUrl() {
 	const dbUrl = process.env.JAWSDB_MARIA_URL || process.env.JAWSDB_URL;
 	if (dbUrl) {
 		const url = new URL(dbUrl);
-console.log(url);
 		return {
 			host: url.hostname,
 			port: parseInt(url.port, 10),
