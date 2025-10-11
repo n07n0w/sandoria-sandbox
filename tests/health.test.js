6256dddcf8084e89b66331bc9df37a89b4646d3f
Ivan Kislov <ivan_kislov@epam.com>
2025-10-11 12:31:13 +0200

    ai-ci-cd-2

---
+++ tests/health.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const express = require('express');
+
+describe('Health Check', () => {
+  it('should pass a basic test', () => {
+    expect(true).toBe(true);
+  });
+});
+
