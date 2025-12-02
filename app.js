5152e9a371557ca863ee31eb486fceb7920f2b66
Vasyl Shevchuk <vasyl@nuvolagroup.com.au>
2025-12-02 21:33:04 +0200

    Rollback image loading optimizations

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('./console-logger');
 var createError = require('http-errors');
 var express = require('express');
-const compression = require('compression');
+//const compression = require('compression');
 const http = require('http');
 const WebSocket = require('ws');
 const session = require('express-session');
@@ -34,10 +34,12 @@ app.locals.peerserversecure = constants.PEER_SERVER_SECURE;
 app.set('views', path.join(__dirname, 'views/ejs'));
 app.set('view engine', 'ejs');
 
+/*
 app.use(compression({
     level: 6,      // compression level (0–9)
     threshold: 0   // compress everything
 }));
+*/
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -64,10 +66,12 @@ app.use(
   }),
 );
 
+/*
 app.use('/images/svg', express.static('public/images/svg', {
     maxAge: '1d',
     immutable: true
 }));
+*/
 
 // Don't set up routes until database is initialized
 let routesInitialized = false;
